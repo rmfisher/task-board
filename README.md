05831bcb478559a10367eb9b24a509b681c83738
Robert Fisher <fisher@tum.de>
2019-04-07 10:59:35 +1200

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# task-board
-
-A 2D task board in the style of Asana or JIRA.
-
-This is not a fully functional app. I am mostly interested in the drag & drop behavior, animation, scrolling etc.
---
+++ precache-manifest.c5fc571a2309d40986d412e6d8744a27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f363ba395d9ee2308ab266ff5b2c95db",
+    "url": "/static/media/sorcerio.f363ba39.png"
+  },
+  {
+    "revision": "bc64689cf399f22336557c60fb3f849e",
+    "url": "/static/media/elfo.bc64689c.png"
+  },
+  {
+    "revision": "361017f1f8c81e80ac313105e426a533",
+    "url": "/static/media/bean.361017f1.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f137b214025426719867",
+    "url": "/static/js/main.0215d805.chunk.js"
+  },
+  {
+    "revision": "a1ebfb0d157efac47b5b",
+    "url": "/static/js/2.dab16cac.chunk.js"
+  },
+  {
+    "revision": "f137b214025426719867",
+    "url": "/static/css/main.e0c65104.chunk.css"
+  },
+  {
+    "revision": "cbe10843f1c002afd311aa8aed5d6675",
+    "url": "/index.html"
+  }
+];
